282dc6b906708f9a6b7d4bd5e60fb9291e5b9aa7
Bba <adel.ahnaf2011@gmail.com>
2025-12-30 19:48:59 +1100

    Initial commit

---
+++ src/main/java/com/bba/allied/AlliedDataGenerator.java
@@ -0,0 +1,11 @@
+package com.bba.allied;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AlliedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
